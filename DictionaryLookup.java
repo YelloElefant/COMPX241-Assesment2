9021ae143e14faa784cf8e58853e0bb73d8e526a
YelloElefant <alextrotternz@gmail.com>
2024-04-09 22:59:44 +0000

    adding DictionaryLookup with main method

---
+++ DictionaryLookup.java
@@ -0,0 +1,10 @@
+public class DictionaryLookup {
+    public static void main(String[] args) {
+        DictionaryBST bst = new DictionaryBST();
+        bst.insert("a", "1");
+        bst.insert("b", "2");
+
+        bst.printDictionary(); // true
+
+    }
+}
